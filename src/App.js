f12c80b86474c9862308b1e48aa7228434c6443e
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-12-03 22:02:08 +0800

    Add Line Component, and repeat Line according to number

--- src/App.js
+++ src/App.js
@@ -2,25 +2,35 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Number from './components/Number.js'
+import Line from './components/Line.js'
 
 class App extends Component {
-  state = {number:0}
-  add1toNumber = () => {
-    this.state.number=this.props.clickedFunction(this.state.number);
-    this.setState({number:this.state.number});
-} 
+  state = {
+    number:0, 
+    numberArray: new Array(0).fill(0)
+  }
+
+  updateNumber = () => {
+    let number = this.props.clickedFunction(this.state.number);
+    this.setState({
+      number: number,
+      numberArray: new Array(number).fill(0)
+    })
+  }
+  
+
 
   render() {
     return (
       <div className="">
-      <button onClick = {this.add1toNumber}>click {this.props.who}</button>
+      <button onClick = {this.updateNumber}>click {this.props.who}</button>
       <span><Number PassedNumber={this.state.number}/></span>
-   
+      {this.state.numberArray.map(() => <Line/>)}
       </div> 
     );
   }
 }
 
-//Number:{this.state.number}
+
 
 export default App;
