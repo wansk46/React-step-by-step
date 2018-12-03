f89023a1f14739eea9241fd14ea0828c91505262
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-12-03 16:12:47 +0800

    [wansk]feat:App will increase number by 2 on every button click

--- src/App.js
+++ src/App.js
@@ -4,17 +4,16 @@ import './App.css';
 
 class App extends Component {
   state = {number:0}
-  alertUser = () => {alert("now you know how to handle onclick event")}
   add1toNumber = () => {
-    this.state.number++;
+    this.state.number=this.props.clickedFunction(this.state.number);
     this.setState({number:this.state.number});
 }
 
   render() {
     return (
       <div className="">
-      <button onClick ={this.props.clickedFunction}>clickedFunction</button>
-      <span>number:{this.props.defaultNum}</span>
+      <button onClick = {this.add1toNumber}>click {this.props.who}</button>
+      <span>Number:{this.state.number}</span>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App defaultNum={10} clickedFunction={() => {alert("you called clickedFunction")}} />, document.getElementById('root'));
+ReactDOM.render(<App who={"us"} clickedFunction={(number) => number+2} />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
