09ccd1557139c117ec45dbc6e51ce22cd9b874ba
SKITTLE WAN (INFRA-ISD-OOCLL/HKG) <WANSK@OOCL.COM>
2018-12-03 14:55:34 +0800

    [WANSK]feat: click the Hello world! button, then I can see the text number: 1

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {number:0}
+  alertUser = () => {alert("now you know how to handle onclick event")}
+  add1toNumber = () => {
+    this.state.number++;
+    this.setState({number:this.state.number});
+}
+
   render() {
     return (
       <div className="">
-      <button>Hello world!</button>
-      <span>number:0</span>
+      <button onClick ={this.add1toNumber}>Hello world!</button>
+      <span>number:{this.state.number}</span>
       </div>
     );
   }
